b83f7588ffe05461c530a4aba52d5591fc57b97d
gianluca <gianluca.giacchetta@gmail.com>
2024-06-10 08:48:33 +0200

    creazione cartella front back

---
+++ gestionaleBack/src/test/java/gestionaleAziendale/gestionale/GestionaleApplicationTests.java
@@ -0,0 +1,13 @@
+package gestionaleAziendale.gestionale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
