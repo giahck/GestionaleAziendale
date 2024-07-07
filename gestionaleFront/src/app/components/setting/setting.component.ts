6da0692e5863e2bd7e4a262c1cbbb292a2d04eaa
gianluca <gianluca.giacchetta@gmail.com>
2024-07-07 02:54:16 +0200

    eseguito modifiche home machine chat e fine creazione tiket

--- gestionaleFront/src/app/components/setting/setting.component.ts
+++ gestionaleFront/src/app/components/setting/setting.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../service/users.service';
 
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.scss'
 })
-export class SettingComponent {
+export class SettingComponent implements OnInit {
+ user!: any;
+constructor(private userSrv:UsersService) { 
+}
+  ngOnInit(): void {
+    this.user=this.userSrv.getUserIdFromLocalStorage();
+  }
 
 }
